5e988da2c1f4bd479d3cd9ae19817b756ca14b84
kareem saeed <karimsaid026@gmail.com>
2022-05-04 18:02:28 +0200

    UI: Adding Common Equations. Make UI Usable.

--- PlotterFunction/UserInterface.py
+++ PlotterFunction/UserInterface.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.ttk import Combobox
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from PlotStatus import PlotStatus
@@ -23,6 +24,8 @@ class UserInterface:
         self.equation = StringVar(self.root, "x")
         self.minLimit = StringVar(self.root, "-10")
         self.maxLimit = StringVar(self.root, "10")
+        self.selected_equation = StringVar(self.root, "")
+        self.common_equations = ('X', 'X^2', 'X^3', '5 / (x^2)', '5', '(x+5) / (x)')
 
     def showErrorMessage(self):
         ms.showerror("ERROR: InValid Input", self.status_message)
@@ -64,12 +67,12 @@ class UserInterface:
         l.place(x=20, y=415)
 
         e = Entry(self.root, textvariable=self.equation, width=40)
-        e.config(font=('Comic Sans MS bold', 10), fg="#000", bg="#FFF")
+        e.config(font=('Comic Sans MS bold', 10), fg="#008000", bg="#FFF")
         e.place(x=100, y=417)
 
         # Create The Plot Button
-        b = Button(self.root, text="Plot", width=30, command=self.plotFunction)
-        b.config(font=('Comic Sans MS bold', 12), fg="#000000", bg="#FFF")
+        b = Button(self.root, text="PLOT", width=30, command=self.plotFunction)
+        b.config(font=('Comic Sans MS bold', 12), fg="#FF0000", bg="#FFF")
         b.place(x=450, y=410)
 
         # Create Min Limit Label & Entry
@@ -80,7 +83,7 @@ class UserInterface:
         l.place(x=20, y=465)
 
         e = Entry(self.root, textvariable=self.minLimit)
-        e.config(font=('Comic Sans MS bold', 10), fg="#000", bg="#FFF")
+        e.config(font=('Comic Sans MS bold', 10), fg="#008000", bg="#FFF")
         e.place(x=120, y=467)
 
         # Create Max Limit Label & Entry
@@ -91,7 +94,23 @@ class UserInterface:
         l.place(x=20, y=510)
 
         e = Entry(self.root, textvariable=self.maxLimit)
-        e.config(font=('Comic Sans MS bold', 10), fg="#000", bg="#FFF")
+        e.config(font=('Comic Sans MS bold', 10), fg="#008000", bg="#FFF")
         e.place(x=120, y=512)
 
+        # Create Max Limit Label & Entry
+        label = StringVar()
+        l = Label(self.root, textvariable=label)
+        label.set("Common Equations: ")
+        l.config(font=('Comic Sans MS bold', 10), fg="#000", bg="#DECBA4")
+        l.place(x=450, y=465)
+
+        equation_choose = Combobox(self.root, width=20, font=('Comic Sans MS bold', 10), textvariable=self.selected_equation)
+        equation_choose.place(x=578, y=467)
+        equation_choose['values'] = self.common_equations
+        equation_choose.current(0)
+        equation_choose.bind("<<ComboboxSelected>>", self.selectedEquation)
+
         self.root.mainloop()
+
+    def selectedEquation(self, event):
+        self.equation.set(self.selected_equation.get())
